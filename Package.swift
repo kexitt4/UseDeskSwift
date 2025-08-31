00afa237084641a0750c5d7a3db656d3f5a8423f
kexitt4 <71172824+kexitt4@users.noreply.github.com>
2025-09-01 01:30:55 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "UseDeskSDKSwift",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v14)
     ],
     products: [
         .library(
@@ -13,13 +13,23 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.7.0")
+        .package(url: "https://github.com/socketio/socket.io-client-swift.git", from: "16.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.7.0"),
+        .package(url: "https://github.com/indragiek/MarkdownKit.git", from: "1.0.0"),
+        .package(url: "https://github.com/TextureGroup/Texture.git", from: "3.0.0"),
+        .package(url: "https://github.com/ashleymills/Reachability.swift.git", from: "5.0.0"),
+        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.6.0")
     ],
     targets: [
         .target(
             name: "UseDeskSDKSwift",
             dependencies: [
-                "Alamofire"
+                .product(name: "SocketIO", package: "socket.io-client-swift"),
+                "Alamofire",
+                "MarkdownKit",
+                "Texture",
+                "Reachability",
+                "SwiftSoup"
             ],
             path: "Sources/UseDeskSDKSwift",
             resources: [
